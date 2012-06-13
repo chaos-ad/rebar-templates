2bbc2c7b8a16fe8fb032db48404147e65a2190b0
Anatoly Kanivetskiy <Chaos.A.D88@gmail.com>
2012-06-13 16:32:45 +0400

    Remove stop function from gen_server template

--- srv/srv.erl
+++ srv/srv.erl
@@ -7,7 +7,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 %% api:
--export([start_link/0, stop/0, stop/1]).
+-export([start_link/0]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -18,19 +18,10 @@
 start_link() ->
     gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 
-stop() ->
-    stop(normal).
-
-stop(Reason) ->
-    gen_server:call(?SERVER, {stop, Reason}).
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 init([]) ->
     {ok, undefined}.
-
-handle_call({stop, Reason}, _, State) ->
-    {stop, Reason, ok, State};
 
 handle_call(_, _, State) ->
     {noreply, ok, State}.
