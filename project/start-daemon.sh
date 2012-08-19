b34ee9657d6c0abbe2e2823d44420bce2f61027e
Anatoly Kanivetskiy <Chaos.A.D88@gmail.com>
2012-08-20 00:33:51 +0200

    Remove shell scripts in project template

--- project/start-daemon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-exec erl -pa ebin deps/*/ebin -boot start_sasl -config priv/app.config -sname {{name}}srv@`hostname` -cookie {{name}}_cookie -detached -s {{name}}
--- project/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-exec erl -pa ebin deps/*/ebin -boot start_sasl -config priv/app.config -s {{name}}
